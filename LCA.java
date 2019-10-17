b2ca484caa7dc0b72a2ae3dc6b100592c77f65d9
Dervla Brennan <brennd19@tcd.ie>
2019-10-17 13:29:27 +0100

    Put DAG code and tests into LCA file

--- LCA.java
+++ LCA.java
@@ -6,11 +6,11 @@
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class LCA 
 {
-    //testing branching
-
     // A Binary Tree node
     public static class Node 
     {
@@ -30,18 +30,18 @@ public class LCA
         private List<Integer> path2 = new ArrayList<>();
 
         // Finds the path from root node to given root of the tree.
-        int findLCA(int n1, int n2) 
+        Integer findLCA(int n1, int n2) 
         {
             path1.clear();
             path2.clear();
             return findLCAInternal(root, n1, n2);
         }
 
-        private int findLCAInternal(Node root, int n1, int n2) 
+        private Integer findLCAInternal(Node root, int n1, int n2) 
         {
             if (!findPath(root, n1, path1) || !findPath(root, n2, path2)) 
             {
-                return -1;
+                return null;
             }
             int i;
             for (i = 0; i < path1.size() && i < path2.size(); i++) 
@@ -82,4 +82,169 @@ public class LCA
             return false;
         }
     }
+   
+  	public static class DAG
+  	{
+  		private final int V; //number of vertices
+  		private final ArrayList<Integer>[] adj;
+  		private final ArrayList<Integer>[] reverseAdj;
+  		
+  		public DAG(int V)
+  		{
+  			this.V = V;
+  	  		adj = (ArrayList<Integer>[]) new ArrayList[V];
+  	  		reverseAdj = (ArrayList<Integer>[]) new ArrayList[V];
+  	  		for (int v = 0; v < V; v++)
+  	  		{
+  	  			adj[v] = new ArrayList<Integer>();
+  	  			reverseAdj[v] = new ArrayList<Integer>();
+  	  		}
+  		}
+  		
+  		//return number of vertices
+  		public int V()
+  		{ return V;}
+
+  		//return list of vertices pointing from v
+  		public ArrayList<Integer> adj(int v)
+  		{ return adj[v]; }
+
+  		//return reversed list of vertices pointing from v
+  		public ArrayList<Integer> reverseAdj(int v)
+  		{ return reverseAdj[v]; }
+  		
+  		//void addEdge(int v, int w) add a directed edge v->w 
+  		public boolean addEdge(int v, int w)
+  		{
+  			// check that new edge is within dag boundaries
+  			if(v >= this.V || w >= this.V || v < 0 || w < 0)
+  			{
+  				return false;
+  			}
+  			// check that new edge doesn't create self loop/directed cycle
+  			if(v != w && !hasPath(w, v) && !adj[v].contains(w))
+  			{
+  				adj[v].add(w);
+  				reverseAdj[w].add(v);
+  				return true;
+  			}
+  			else
+  			{
+  				return false;
+  			}
+  		}
+  		
+  		//returns true if has path, else false
+  		public boolean hasPath(int x, int y)
+  		{
+  			DFS dfs = new DFS(this, x);
+  			return dfs.visited(y);
+  		}
+
+  		//returns list of lcas of two vertices x and y
+  		public ArrayList<Integer> getLCA(int x, int y)
+  		{
+  			ArrayList<Integer> lcas = new ArrayList<Integer>();
+  			int max = Integer.MAX_VALUE;
+  			//return null if invalid
+  			if(x == y || x >= this.V || y >= this.V || x < 0 || y < 0) 
+  			{ 
+  				return null; 
+  			} 
+  			DFS dfs= new DFS(this, x);
+  			dfs.reverseDFS(this, x);
+  			int xDist, yDist;
+  		
+  			for(int v = 0; v < this.V; v++)
+  			{
+  				if(dfs.revVisited(v) && hasPath(v, y))
+  				{
+  					xDist = getDistance(v, x);
+  					yDist = getDistance(v, y);
+  					if(Integer.max(xDist, yDist) == max)
+  					{
+  						lcas.add(v);
+  						max = Integer.max(xDist, yDist);
+  					}
+  					else if(Integer.max(xDist, yDist) < max)
+  					{		
+  						lcas = new ArrayList<Integer>();
+  						lcas.add(v);
+  						max = Integer.max(xDist, yDist);
+  					}
+  				}
+  			}
+  			return lcas;
+  		}
+
+  		public int getDistance(int x, int target)
+  		{
+  			if( x == target) 
+  			{ 
+  				return 0; 
+  			}
+  			else 
+  			{
+  		        Queue<Integer> q = new LinkedList<Integer>();
+  		        int[] distTo = new int[this.V];
+  		        boolean[] marked = new boolean[this.V];
+  		        for (int v = 0; v < this.V(); v++)
+  		        {   
+  		        	distTo[v] = Integer.MAX_VALUE;
+  		        }
+  		        distTo[x] = 0;
+  		        marked[x] = true;
+  		        q.add(x);
+  		        while (!q.isEmpty()) 
+  		        {
+  		            int v = q.remove();
+  		            for (int w : this.adj(v))
+  		            {
+  		                if (!marked[w])
+  		             	{
+  		                	distTo[w] = distTo[v] + 1;
+  		                    marked[w] = true;
+  		                    q.add(w);
+  		                }
+  		            }
+  		        }
+  		        return distTo[target];
+  			}
+  		}
+
+  		//Class to create a DFS object on a DAG
+  		private class DFS
+  		{
+  			private boolean[] marked;
+  			private boolean[] revMarked;
+  			public DFS(DAG G, int s)
+  			{
+  				marked = new boolean[G.V()];
+  				revMarked = new boolean[G.V()];
+  				DFS(G, s);
+  			}
+  			//standard dfs - in flow of direction
+  			private void DFS(DAG G, int v)
+  			{
+  				marked[v] = true;
+  				for (int w : G.adj(v))
+  					if (!marked[w]) 
+  						DFS(G, w);
+  			}
+  			//dfs against flow of direction - to find all parents
+  			private void reverseDFS(DAG G, int v)
+  			{
+  				revMarked[v] = true;
+  				for (int w : G.reverseAdj(v))
+  					if (!revMarked[w]) 
+  						reverseDFS(G, w);
+  			}
+  			public boolean visited(int v)
+  			{ return marked[v]; }
+  			public boolean revVisited(int v)
+  			{ return revMarked[v]; }
+  		}
+  		
+  	}
 }
+
--- LCATest.java
+++ LCATest.java
@@ -5,25 +5,28 @@ import org.junit.Test;
 public class LCATest 
 {
 	/**
-	 * 				 1
-	 * 			   /   \
-	 * 		     2		 3
-	 * 		   /  \     /  \
-	 * 		  4    5   6    7
+	 * Test for BT LCA
 	 */
 	@Test
-	public void test() 
+	public void testBTLCA() 
 	{
+		/*
+		 * 				 1
+		 * 			   /   \
+		 * 		     2		 3
+		 * 		   /  \     /  \
+		 * 		  4    5   6    7
+		 */
 		LCA.BT tree = new LCA.BT();
-
-		//test on empty tree
-		assertEquals(tree.findLCA(0, 0), -1);
 		
-		tree.root = new LCA.Node(1);
+		//test on empty tree
+		assertNull(tree.findLCA(0, 0));
+		
+		tree.root = new LCA.Node(-1);
 		tree.root.left = new LCA.Node(2);
 		
 		//test on tree consisting of only 2 nodes
-		assertEquals(tree.findLCA(1, 2), 1);
+		assertEquals(tree.findLCA(-1, 2), Integer.valueOf(-1));
 		
 		tree.root.right = new LCA.Node(3);
 		tree.root.left.left = new LCA.Node(4);
@@ -32,17 +35,98 @@ public class LCATest
 		tree.root.right.right = new LCA.Node(7);
 		
 		//test on tree of multiple nodes
-		assertEquals(tree.findLCA(4, 5), 2);
-		assertEquals(tree.findLCA(6, 7), 3);
-		assertEquals(tree.findLCA(2, 7), 1);
+		assertEquals(tree.findLCA(4, 5), (Integer) 2);
+		assertEquals(tree.findLCA(6, 7), (Integer) 3);
+		assertEquals(tree.findLCA(2, 7), Integer.valueOf(-1));
 		
 		//test where lca is one of the nodes itself
-		assertEquals(tree.findLCA(2, 4), 2);
+		assertEquals(tree.findLCA(2, 4), (Integer) 2);
 		//test where one node is not in tree
-		assertEquals(tree.findLCA(50, 2), -1);
+		assertNull(tree.findLCA(50, 2));
 		//test where neither node is in tree
-		assertEquals(tree.findLCA(50, 100), -1);
+		assertNull(tree.findLCA(50, 100));
 	}
 	
+	/**
+	 * Test for DAG
+	 */
+	@Test
+	public void testAddEdge()
+	{
+		LCA.DAG dag = new LCA.DAG(3);
+		assertTrue("Test add edge within boundaries of dag", dag.addEdge(0, 1));
+		assertFalse("Test add edge outside boundaries of dag", dag.addEdge(-1, 2));
+		assertFalse("Test add edge that creates self loop", dag.addEdge(1, 1));
+		dag.addEdge(1, 2);
+		assertFalse("Test add edge that creates directed cycle", dag.addEdge(2, 0));
+	}
+
+	@Test
+	public void testDistance()
+	{
+		LCA.DAG dag = new LCA.DAG(6);
+		//costruct DAG
+		/*
+		 * 				0
+		 * 			/		\
+		 * 		  1		      2
+		 * 		/			/	
+		 * 		3			4
+		 * 	   /
+		 * 	   5
+		 */
+		dag.addEdge(0, 1); dag.addEdge(0, 2); dag.addEdge(1, 3); dag.addEdge(2, 4); dag.addEdge(3, 5);
+		assertEquals("Test root distance is 0", 0, dag.getDistance(0, 0));
+		assertEquals("Test distance 1", 1, dag.getDistance(0, 1));
+		assertEquals("Test distance 2", 2, dag.getDistance(0, 4));
+		assertEquals("Test distance within dag", 2, dag.getDistance(1, 5));
+	}
+	
+	@Test 
+	public void testDFS()
+	{
+		
+	}
+	
+	@Test
+	public void testHasPath()
+	{
+		LCA.DAG dag = new LCA.DAG(6);
+		/*
+		 * 			 0
+		 * 		  /		\
+		 * 		1		  2
+		 * 	  /	 \	     /  
+		 *   5	  \     3
+		 *   	   \   /
+		 *   		 4
+		 */
+		//construct DAG
+		dag.addEdge(0,1);dag.addEdge(0,2);dag.addEdge(1,5);dag.addEdge(1,4);dag.addEdge(2,3);dag.addEdge(3, 4);
+		assertTrue("Test where path exists", dag.hasPath(0, 1));
+		assertFalse("Test where path doesn't exist", dag.hasPath(5, 4));
+	}
+	
+	@Test
+	public void testGetLCA()
+	{
+		/*
+		 * 			   0	  1
+		 * 		  		\   /	\
+		 * 		      2	   3     4	
+		 * 		 	 \	\ / \	/
+		 * 			  \	 /\	 \ /
+		 * 			   5   \ \/ 
+		 * 					 6
+		 */
+		//construct DAG		
+		LCA.DAG dag = new LCA.DAG(7);
+		dag.addEdge(0,3);dag.addEdge(1,3);dag.addEdge(1,4);dag.addEdge(3,5);dag.addEdge(3,6);dag.addEdge(2,5);
+		dag.addEdge(2,6);dag.addEdge(4,6);
+		assertNull("Test where invalid request", dag.getLCA(7,1));
+		assertEquals("Test where only one lca", (Integer) 1 , dag.getLCA(3,4).get(0));
+		assertEquals("Test where multiple lcas", (Integer) 2, dag.getLCA(5, 6).get(0));
+		assertEquals("Test where multiple lcas", (Integer) 3, dag.getLCA(5, 6).get(1));
+	}
 
 }
